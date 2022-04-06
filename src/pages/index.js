8b4cc90acde1dcad11fe5b7ed248cc1da46dc4e5
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 01:27:56 +0900

    Add src/pages/index.js

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+// React のインポート． .js ファイル内で JSX を使用できる．
+import React from "react"
+
+// React コンポーネントを定義して他の場所で使用できるようにエクスポート．
+// コンポーネント名は大文字で始める．
+const Home = () => {
+    // JSX element を返す (複数は不可能，入れ子は可能)
+    return (
+        <main>
+            <title>Home Page</title>
+            <h1>Hello Gatsby!</h1>
+        </main>
+    )
+}
+
+export default Home
