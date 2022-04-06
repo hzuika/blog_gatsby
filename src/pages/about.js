dc95049925aa8855442b7467cc70d362f664eaa6
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 02:51:19 +0900

    Add react component with props argument

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,22 @@
 // ファイル名 がURLのパスになる．
 import React from "react"
 
+// props でタグの引数を使用できる．
+const Greeting = (props) => {
+    return (
+        <p>Hi {props.name}!</p>
+    )
+}
+
 const AboutPage = () => {
     return (
         <main>
             <title>New Page</title>
             <h1>About Me</h1>
+            <div>
+                <Greeting name="Taro" />
+                <Greeting name="Hanako" />
+            </div>
         </main>
     )
 }
