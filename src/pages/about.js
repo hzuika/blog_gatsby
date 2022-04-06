bd89962a42215dd9ea3fba363a11211e1bfb9134
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 02:31:02 +0900

    Add New about.js page

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+// ファイル名 がURLのパスになる．
+import React from "react"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>New Page</title>
+            <h1>About Me</h1>
+        </main>
+    )
+}
+
+export default AboutPage
