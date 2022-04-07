73721b7dc724ea26c406b590f59dc4e1775a4df6
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 22:00:33 +0900

    Render mdx files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,26 +1,34 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
-      <p>My cool posts will go in here</p>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <MDXRenderer>{node.body}</MDXRenderer>
+        </article>
+      ))}
     </Layout>
   );
 };
 
-// gatsby-config.js で設定したディレクトリ内のファイル名を取得する
+// gatsby-config.js で設定したディレクトリ内の mdx ファイルの情報を取得
+// frontmatter の date を 降順(新しい順)で取得
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        body
       }
     }
   }
