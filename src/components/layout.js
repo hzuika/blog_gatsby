ac692912af9bd3dcd479bc5e675a6bb89d09e9a2
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 11:56:09 +0900

    Add layout.module.css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import { 
+    container, 
+    heading, 
+    navLinks, 
+    navLinkItem, 
+    navLinkText 
+} from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
     // 引数を props にして，次のように書くことと同じ．
     // const pageTitle = props.pageTitle
     // const children = props.children
     return (
-        <div>
+        <div className={container}>
             <title>{pageTitle}</title>
             <nav>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
+                <ul className={navLinks}>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>
+                            About
+                        </Link>
+                    </li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
