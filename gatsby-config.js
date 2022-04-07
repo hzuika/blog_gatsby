4272f00afea6953c09642374a1d9d967092adba7
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-07 20:45:05 +0900

    Get file data by graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,16 @@ module.exports = {
         "gatsby-plugin-image",
         // 画像処理用
         "gatsby-plugin-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            // ビルド時に options で指定したフォルダ内の全てのファイルを path データレイヤーに追加する
+            options: {
+                // name オプションは各ファイルの sourceInstanceName に設定される
+                // GraphQL のフィルタとして使用できる
+                name: `blog`,
+                // __dirname は実行中のファイルを含むディレクトリの絶対パス
+                path: `${__dirname}/blog`,
+            },
+        },
     ]
 }
