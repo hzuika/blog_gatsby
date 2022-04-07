519676d34bf5b3a820ad1b6c4252cc74caad0441
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-08 03:54:51 +0900

    Add outputTheme.js
    prism-react-renderer/tools/themeFromVsCode
    vscode dark+ のテーマを使用したが，色が違う気がする．

---
+++ src/components/outputTheme.js
@@ -0,0 +1,81 @@
+// @flow
+// Converted automatically using prism-react-renderer/tools/themeFromVsCode
+// From vscode dark+ json color theme
+// 色が違う気がする
+
+var theme = {
+  plain: {
+    color: "#d4d4d4",
+    backgroundColor: "#1e1e1e",
+  },
+  styles: [
+    {
+      types: ["prolog"],
+      style: {
+        color: "rgb(0, 0, 128)",
+      },
+    },
+    {
+      types: ["comment", "punctuation"],
+      style: {
+        color: "rgb(106, 153, 85)",
+      },
+    },
+    {
+      types: ["builtin"],
+      style: {
+        color: "rgb(79, 193, 255)",
+      },
+    },
+    {
+      types: ["number", "variable", "inserted"],
+      style: {
+        color: "rgb(181, 206, 168)",
+      },
+    },
+    {
+      types: ["operator"],
+      style: {
+        color: "rgb(212, 212, 212)",
+      },
+    },
+    {
+      types: ["constant"],
+      style: {
+        color: "rgb(100, 102, 149)",
+      },
+    },
+    {
+      types: ["tag", "changed", "function", "keyword"],
+      style: {
+        color: "rgb(86, 156, 214)",
+      },
+    },
+    {
+      types: ["attr-name"],
+      style: {
+        color: "rgb(156, 220, 254)",
+      },
+    },
+    {
+      types: ["deleted", "string"],
+      style: {
+        color: "rgb(206, 145, 120)",
+      },
+    },
+    {
+      types: ["class-name"],
+      style: {
+        color: "rgb(78, 201, 176)",
+      },
+    },
+    {
+      types: ["char"],
+      style: {
+        color: "rgb(209, 105, 105)",
+      },
+    },
+  ],
+};
+
+module.exports = theme;
