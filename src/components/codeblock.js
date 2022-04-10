28198a395b4c91f4070734b36f0de6f7cdce8757
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2022-04-10 22:56:46 +0900

    Move styled-component in js

--- src/components/codeblock.js
+++ src/components/codeblock.js
@@ -1,8 +1,36 @@
 import React from "react";
-import { Pre, Line, LineNo, LineContent } from "./styles";
 import Highlight, { defaultProps } from "prism-react-renderer";
 import Prism from "prism-react-renderer/prism";
 import theme from "prism-react-renderer/themes/vsDark";
+import styled from "styled-components";
+
+const Pre = styled.pre`
+  text-align: left;
+  margin: 1em 0;
+  padding: 0.5em;
+  overflow: scroll;
+
+  & .token-line {
+    line-height: 1.3em;
+    height: 1.3em;
+  }
+`;
+
+const Line = styled.div`
+  display: table-row;
+`;
+
+const LineNo = styled.span`
+  display: table-cell;
+  text-align: right;
+  padding-right: 1em;
+  user-select: none;
+  opacity: 0.5;
+`;
+
+const LineContent = styled.span`
+  display: table-cell;
+`;
 
 // Add more language support
 // Default: https://github.com/FormidableLabs/prism-react-renderer/blob/master/src/vendor/prism/includeLangs.js
